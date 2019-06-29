86ff1c71e1765b3103a66564391b7e1c18b5a1d2
Dave Brett <david.brett@tessella.com>
2019-06-29 22:31:16 +0100

    initial grpcweb server code

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	pb "github.com/bredr/todo/pb"
+	"github.com/bredr/todo/router"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
+	"google.golang.org/grpc"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	routing := &router.Router{}
+	var opts []grpc.ServerOption
+	s := grpc.NewServer(opts...)
+	pb.RegisterTodoServer(s, routing)
+
+	wrappedGrpc := grpcweb.WrapServer(s)
+	httpSrv := &http.Server{
+		ReadHeaderTimeout: 5 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		Addr:              ":http",
+	}
+	httpSrv.Handler = http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		if wrappedGrpc.IsGrpcWebRequest(req) {
+			wrappedGrpc.ServeHTTP(resp, req)
+		}
+		// Fall back to other servers.
+		http.DefaultServeMux.ServeHTTP(resp, req)
+	})
+	log.Fatal(httpSrv.ListenAndServe())
+}
---
+++ server/router/router.go
@@ -0,0 +1,69 @@
+package router
+
+import (
+	pb "github.com/bredr/todo/pb"
+	"context"
+	"errors"
+	"github.com/google/uuid"
+	"log"
+)
+
+type Router struct {
+	Items []*pb.Item
+}
+
+var _  pb.TodoServer = &Router{}
+
+func (r *Router) GetItem(ctx context.Context, req *pb.ID) (*pb.Item, error) {
+	log.Printf("GetItem::%v", req)
+	for _, v := range r.Items {
+		if v.ID == req.ID {
+			return v, nil
+		}
+	}
+	return nil, errors.New("Not found")
+}
+
+func (r *Router) GetItems(ctx context.Context, req *pb.Pagination) (*pb.Items, error) {
+	log.Printf("GetItems::%v", req)
+	if len(r.Items) == 0 || int(req.Offset) >= len(r.Items) {
+		return nil, nil
+	}
+	end := int(req.Limit + req.Offset)
+	if end > len(r.Items) {
+		end = len(r.Items)
+	}
+	return &pb.Items{Items: r.Items[req.Offset:end], Total: int32(len(r.Items)), Offset: req.Offset, Limit: int32(end) - req.Offset}, nil
+}
+
+func (r *Router) RemoveItem(ctx context.Context, req *pb.ID) (*pb.Empty, error) {
+	log.Printf("RemoveItem::%v", req)
+	new := r.Items[:0]
+	for i, v := range r.Items {
+		if v.ID != req.ID {
+			new[i] = v
+		}
+	}
+	r.Items = new
+	return &pb.Empty{}, nil
+}
+
+func (r *Router) UpsertItem(ctx context.Context, req *pb.Item) (*pb.ID, error) {
+	log.Printf("UpsertItem::%v", req)
+	if req.ID == "" {
+		ID, err := uuid.NewRandom()
+		if err != nil {
+			return nil, err
+		}
+		req.ID = ID.String()
+		r.Items = append(r.Items, req)
+		return &pb.ID{ID: req.ID}, nil
+	}
+	for i, v := range r.Items {
+		if v.ID == req.ID {
+			r.Items[i] = req
+			return &pb.ID{ID: req.ID}, nil
+		}
+	}
+	return nil, errors.New("Unknown ID")
+}
